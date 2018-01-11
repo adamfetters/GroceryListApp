1899496db5b9833d70b90d43f059d1be1740c5f8
Adam Fetters <adamfetterscoder@gmail.com>
2018-01-11 16:33:40 -0500

    Working on Store Component

--- client/src/components/StoreSelection/StoreSelectionTable.js
+++ client/src/components/StoreSelection/StoreSelectionTable.js
@@ -28,16 +28,11 @@ class StoreSelectionTable extends Component {
 
   render() {
     return (
-      <div className="header">
-        {this.state.stores
-          ? this.state.stores.map((store, i) => (
-              <div className="store" key={i}>
-                <div className="store-name">
-                  <b>{store.name}</b>
-                </div>
-              </div>
-            ))
-          : null}
+      <div>
+        <div className="header">Stores</div>
+        <div>
+          {this.state.stores ? this.state.stores.map((store, index) => <Store key={index} store={store} />) : null}
+        </div>
       </div>
     );
   }
